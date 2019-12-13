aa435e3b567b61986f73b9d1d95020fd8d5ee02f
Alethia Quintero <alethiaq22@gmail.com>
2019-12-13 18:32:00 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 class App extends Component {
  
   handleOnClickItems() {
-    this.props.store.dispatch({
+ return (   this.props.store.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
-    });
+    });)
   }
  
   handleOnClickUsers() {
